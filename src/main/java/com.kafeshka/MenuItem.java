e322f7ea200723d40296c1cb638818b2a19f7be9
ninastarovojtova <ninastarowoitowa@gmail.com>
2023-10-09 00:20:01 +0200

    add lombok

--- src/main/java/com.kafeshka/MenuItem.java
+++ src/main/java/com.kafeshka/MenuItem.java
@@ -1,5 +1,10 @@
 package com.kafeshka;
 
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
 public class MenuItem {
     private String name;
     private double price;
@@ -9,72 +14,4 @@ public class MenuItem {
     private boolean spicy;
     private int calories;
 
-    public MenuItem(String name, double price, double weightOrValue, String description, boolean vegetarian, boolean spicy, int calories) {
-        this.name = name;
-        this.price = price;
-        this.weightOrValue = weightOrValue;
-        this.description = description;
-        this.vegetarian = vegetarian;
-        this.spicy = spicy;
-        this.calories = calories;
-    }
-
-    public MenuItem() {
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public double getPrice() {
-        return price;
-    }
-
-    public void setPrice(double price) {
-        this.price = price;
-    }
-
-    public double getWeightOrValue() {
-        return weightOrValue;
-    }
-
-    public void setWeightOrValue(double weightOrValue) {
-        this.weightOrValue = weightOrValue;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public boolean isVegetarian() {
-        return vegetarian;
-    }
-
-    public void setVegetarian(boolean vegetarian) {
-        this.vegetarian = vegetarian;
-    }
-
-    public boolean isSpicy() {
-        return spicy;
-    }
-
-    public void setSpicy(boolean spicy) {
-        this.spicy = spicy;
-    }
-
-    public int getCalories() {
-        return calories;
-    }
-
-    public void setCalories(int calories) {
-        this.calories = calories;
-    }
 }
