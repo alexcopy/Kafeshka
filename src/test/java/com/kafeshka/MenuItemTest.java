4564c06154ee7ee2bff104b2beb5de9076f26a4e
ninastarovojtova <ninastarowoitowa@gmail.com>
2023-10-09 00:08:18 +0200

    add tests

---
+++ src/test/java/com/kafeshka/MenuItemTest.java
@@ -0,0 +1,21 @@
+package com.kafeshka;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MenuItemTest {
+    @Test
+    void testMenuItem() {
+        MenuItem menuItem = new MenuItem("Margarita", 2.13, 500, "italien pizza", false, true, 5000);
+        assertEquals("Margarita", menuItem.getName());
+        assertEquals("italien pizza", menuItem.getDescription());
+        assertEquals(2.13, menuItem.getPrice());
+        assertEquals(500, menuItem.getWeightOrValue());
+        assertEquals(5000, menuItem.getCalories());
+        assertTrue(menuItem.isSpicy());
+        assertFalse(menuItem.isVegetarian());
+
+
+    }
+}
