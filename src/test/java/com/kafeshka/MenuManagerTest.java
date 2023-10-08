80347df939d55c881cefdc5d3fca0b210595068e
ninastarovojtova <ninastarowoitowa@gmail.com>
2023-10-09 00:32:08 +0200

    add test

---
+++ src/test/java/com/kafeshka/MenuManagerTest.java
@@ -0,0 +1,28 @@
+package com.kafeshka;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MenuManagerTest {
+    private MenuManager menuManager;
+
+    @BeforeEach
+    void setUp() {
+        this.menuManager = new MenuManager();
+    }
+
+    @Test
+    void addMenuItem() {
+        MenuItem menuItem = new MenuItem("Margarita", 2.13, 500, "italien pizza", false, true, 5000);
+        this.menuManager.addMenuItem(menuItem);
+        assertTrue(this.menuManager.getMenuItems().contains(menuItem));
+    }
+
+    @Test
+    void removeMenuItem() {
+        MenuItem menuItem = new MenuItem("Margarita", 2.13, 500, "italien pizza", false, true, 5000);
+ //ToDo add test here
+    }
+}
