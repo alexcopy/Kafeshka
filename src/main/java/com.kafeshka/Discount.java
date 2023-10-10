a3913d15ef4fbee125889cff25e569180901c072
ninastarovojtova <ninastarowoitowa@gmail.com>
2023-10-11 00:01:36 +0200

    add class order and workflow

---
+++ src/main/java/com.kafeshka/Discount.java
@@ -0,0 +1,4 @@
+package com.kafeshka;
+
+public class Discount {
+}
--- src/main/java/com.kafeshka/Order.java
+++ src/main/java/com.kafeshka/Order.java
@@ -1,20 +1,39 @@
 package com.kafeshka;
 
 
+import java.util.Date;
 import java.util.List;
 
 public class Order {
-     /*List<MenuItem>items;
-     items = new ArrayList ();
+    private List<MenuItem> items;
+    private double totalAmount;
+    private double tips;
+    private String orderComments;
+    private Date orderDate;
+    private boolean delivery;
+    private Date deliveryTime;
+    private String deliveryAddress;
+    private int orderID;
+    private Customer customer;
+    private OrderStatus status;
+    private Discount discount;
+    private PaymentMethod paymentMethod;
+
+    public Order(List<MenuItem> items, double totalAmount, double tips, String orderComments, Date orderDate, boolean delivery, Date deliveryTime, String deliveryAddress, int orderID, Customer customer, OrderStatus status, Discount discount, PaymentMethod paymentMethod) {
+        this.items = items;
+        this.totalAmount = totalAmount;
+        this.tips = tips;
+        this.orderComments = orderComments;
+        this.orderDate = orderDate;
+        this.delivery = delivery;
+        this.deliveryTime = deliveryTime;
+        this.deliveryAddress = deliveryAddress;
+        this.orderID = orderID;
+        this.customer = customer;
+        this.status = status;
+        this.discount = discount;
+        this.paymentMethod = paymentMethod;
+    }
 
 
-     public Order(List<MenuItem> items) {
-          this.items = items;
-     }
-
-     public int calculateTotalAmount( int sum){
-
-          }
-
-    }*/
 }
