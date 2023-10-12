3f4fe8c49245e79503d2af171d03f073a723559c
ninastarovojtova <ninastarowoitowa@gmail.com>
2023-10-12 23:56:29 +0200

    add new class orderManager and OrderException

--- src/main/java/com.kafeshka/OrderManager.java
+++ src/main/java/com.kafeshka/OrderManager.java
@@ -1,4 +1,28 @@
 package com.kafeshka;
 
-public class OrderManager  {
+import com.kafeshka.exception.OrderException;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class OrderManager {
+    List<Order> orders;
+
+    public OrderManager(List<Order> orders) {
+        orders = new ArrayList<>();
+    }
+
+    public void placeOrder(Order order) throws OrderException {
+        if (order == null || orders.contains(order)) {
+            throw new OrderException("Order is empty or already exists");
+        }
+        orders.add(order);
+    }
+
+    public void cancelOrder(Order order) throws OrderException {
+        if (order == null || !orders.contains(order)){
+            throw new OrderException("Order is empty or order mot exists");
+        }
+        orders.remove(order);
+    }
 }
---
+++ src/main/java/com.kafeshka/exception/OrderException.java
@@ -0,0 +1,7 @@
+package com.kafeshka.exception;
+
+public class OrderException extends Exception {
+    public OrderException(String message) {
+        super(message);
+    }
+}
