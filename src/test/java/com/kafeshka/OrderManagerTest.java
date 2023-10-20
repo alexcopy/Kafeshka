36439681b098cf46e0ea62557a8c9e5721a239b9
ninastarovojtova <ninastarowoitowa@gmail.com>
2023-10-21 00:26:46 +0200

    add some new tests

--- src/test/java/com/kafeshka/OrderManagerTest.java
+++ src/test/java/com/kafeshka/OrderManagerTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.util.Date;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -27,39 +28,65 @@ class OrderManagerTest {
     }
 
     @Test
-    void  testPlaceOrder() throws OrderException {
+    void testPlaceOrder() throws OrderException {
         Order order = new Order(customer, paymentMethod);
         orderManager.placeOrder(order);
-        assertEquals(1,orderManager.getOrders().size() );
+        assertEquals(1, orderManager.getOrders().size());
     }
+
     @Test
-    void  testPlaceOrderWithException()   {
+    void testPlaceOrderWithException() {
 
         try {
             orderManager.placeOrder(null);
         } catch (OrderException e) {
-          assertEquals("Order is empty or already exists", e.getMessage());
+            assertEquals("Order is empty or already exists", e.getMessage());
         }
-        assertEquals(0,orderManager.getOrders().size() );
+        assertEquals(0, orderManager.getOrders().size());
     }
 
     @Test
-    void cancelOrder() {
+    void testCancelOrder() throws OrderException {
+        Order order = new Order(customer, paymentMethod);
+        orderManager.placeOrder(order);
+        assertEquals(1, orderManager.getOrders().size());
+        orderManager.cancelOrder(order);
+        assertEquals(0, orderManager.getOrders().size());
+
     }
 
     @Test
-    void getOrdersByCustomer() {
+    void getOrdersByCustomer() throws OrderException {
+        Order order1 = new Order(customer, paymentMethod);
+        Order order2 = new Order(customer, paymentMethod);
+        Order order3 = new Order(customer, paymentMethod);
+        orderManager.placeOrder(order1);
+        orderManager.placeOrder(order2);
+        orderManager.placeOrder(order3);
+        List<Order> ordersByCustomer = orderManager.getOrdersByCustomer(customer);
+        assertEquals(3, ordersByCustomer.size());
     }
 
     @Test
-    void updateOrderStatus() {
+    void testUpdateOrderStatus() throws OrderException {
+        Order order = new Order(customer, paymentMethod);
+        orderManager.placeOrder(order);
+        orderManager.updateOrderStatus(order, OrderStatus.DELIVERED);
+        assertEquals(OrderStatus.DELIVERED, order.getStatus());
+
     }
 
     @Test
-    void getOrders() {
+    void testGetOrders() throws OrderException {
+        Order order1 = new Order(customer, paymentMethod);
+        Order order2 = new Order(customer, paymentMethod);
+        Order order3 = new Order(customer, paymentMethod);
+        orderManager.placeOrder(order1);
+        orderManager.placeOrder(order2);
+        orderManager.placeOrder(order3);
+        List<Order> orders = orderManager.getOrders();
+        assertEquals(3, orders.size());
+
     }
 
-    @Test
-    void getOrderHistory() {
-    }
 }
