719eebc35a3851a38c9e852573b9103a47996963
Alex Redko <a.redko@mail.com>
2023-11-30 09:29:48 +0000

    fix mysql db for docker

--- src/main/java/com/kafeshka/KafeshkaRS/menu/MenuItem.java
+++ src/main/java/com/kafeshka/KafeshkaRS/menu/MenuItem.java
@@ -1,42 +1,17 @@
 package com.kafeshka.KafeshkaRS.menu;
 
-import jakarta.persistence.*;
 import lombok.*;
 
 @Getter
 @Setter
 @AllArgsConstructor
-@NoArgsConstructor
-@Entity
-@Table(name = "menu_items")
-public class MenuItem {
-
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
-
-    @Column(name = "name")
+public class MenuItem{
     private String name;
-
-    @Column(name = "price")
     private double price;
-
-    @Column(name = "weight_or_value")
     private double weightOrValue;
-
-    @Column(name = "description")
     private String description;
-
-    @Column(name = "vegetarian")
     private boolean vegetarian;
-
-    @Column(name = "spicy")
     private boolean spicy;
-
-    @Column(name = "calories")
     private int calories;
-
-    @Column(name = "cooking_time_sec")
     private int cookingTimeSec;
-
 }
