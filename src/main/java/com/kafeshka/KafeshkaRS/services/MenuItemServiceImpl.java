258736e01a84b32743151ba603d3cf816194d75c
Alex Redko <a.redko@mail.com>
2023-12-06 15:35:19 +0000

    add MenuItem api endpoint

---
+++ src/main/java/com/kafeshka/KafeshkaRS/services/MenuItemServiceImpl.java
@@ -0,0 +1,53 @@
+package com.kafeshka.KafeshkaRS.services;
+
+import com.kafeshka.KafeshkaRS.model.MenuItem;
+import com.kafeshka.KafeshkaRS.repository.MenuItemRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class MenuItemServiceImpl implements MenuItemServiceInt {
+
+    private final MenuItemRepository menuItemRepository;
+
+    @Autowired
+    public MenuItemServiceImpl(MenuItemRepository menuItemRepository) {
+        this.menuItemRepository = menuItemRepository;
+    }
+
+    @Override
+    public List<MenuItem> getAllMenuItems() {
+        return menuItemRepository.findAll();
+    }
+
+    @Override
+    public Optional<MenuItem> getMenuItemById(Long id) {
+        return menuItemRepository.findById(id);
+    }
+
+    @Override
+    public Optional<MenuItem> createMenuItem(MenuItem menuItem) {
+        try {
+            MenuItem createdMenuItem = menuItemRepository.save(menuItem);
+            return Optional.ofNullable(createdMenuItem);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return Optional.empty();
+        }
+    }
+
+    @Override
+    public boolean deleteMenuItem(Long id) {
+        try {
+            menuItemRepository.deleteById(id);
+            return true;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+}
+
